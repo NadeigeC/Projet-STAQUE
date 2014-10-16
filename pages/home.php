c7df20bf8edf622d0e49de7659aaadcc9aad9d69
pablodido <gsaissi@gmail.com>
2014-10-16 21:43:38 +0200

    css et html rajouté,login, home.php fait

--- pages/home.php
+++ pages/home.php
@@ -3,14 +3,68 @@
 <body>
 
 	<div id="wrapper">
+		<nav id="head">
+		
+		<?php	
+			if (userIsLogged()){
+			echo "Yo " . $_SESSION['user']['username'] . "!";
+			echo ' <a href="logout.php" title="Logout !" style="display:block">Logout</a>';
+			}
+		else {
+		?>
+			<a class="login" href="pages/login.php"><button>Log In</button></a>
+		<?php } ?>
+			
+			<h1>Stackoverflone</h1>
+			<input type="text" name="search" value="Search">
+			
+		</nav>
+		<main>
+			<div class="mainContent">
+				<a href="questions.php"><button>Questions</button></a>
+				<a href="askquestions.php"><button>Ask Question</button></a>
+				<a href="logout.php"><button>Déconnexion</button></a>
+				<a class="signup"href="register.php"><button>Sign up</button></a>
+			<div>
+		</main>
+		<footer>
+			<div id="foot">
+				<div class="topfoot">
+					<a href="">help |</a>
+					<a href="">chat |</a>
+					<a href="">data |</a>
+					<a href="">legal |</a>
+					<a href="">privacy policy |</a>
+					<a href="">work here |</a>
+					<a href="">advertising info |</a>
+					<a href="">contact us |</a>
+					<a href="">feedback </a>
+				</div>
+				<div class="contentfoot">
+				<h3>TECHNOLOGY</h3>
+				<div class="leftfoot">
+					<a href="">Stack Overflow</a>
+					<a href="">Server Fault</a>
+					<a href="">Super user</a>
+					<a href="">Web Applications</a>
+					<a href="">Ask Ubuntu</a>
+				</div>
+				<div class="rightfoot">
+					<a href=""> Webmasters</a><br/>
+					<a href="">Game Development</a><br/>
+					<a href="">programmers</a><br/>
+					<a href="">WordPress Development</a><br/>
+					<a href="">Information security</a>
+				</div>
+				</div>
+				<div id="copy">
+					<p>Site design/ logo &copy; <?php echo date("Y"); ?> staque corporation; user  contributions licensed under <a href=""> ccby-sa 3.0</a>
+				</div>	
+			</div>	
+			
+		</footer>
 
 	</div>
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/jquery.min.js">\x3C/script>')</script>
-	<script>
-
-	</script>
-	
 </body>
 </html>
+
---
+++ pages/login.php
@@ -0,0 +1,102 @@
+<?php
+	session_start();
+	
+	include("db.php");
+	include("../functions.php");
+
+	//déclaration des variables du formulaire
+	$username 		= "";
+	$password 		= "";
+
+	$errors = array();
+
+	//formulaire soumis ?
+	if (!empty($_POST)){
+
+		//on écrase les valeurs définies ci-dessus, tout en se protegeant
+		//pas de strip tags sur la password par contre (si la personne veut mettre des balises dans son pw, c'est son affaire, et on le hache anyway)
+		$username 		= strip_tags($_POST['username']);
+		$password 		= $_POST['password'];
+
+		//validation
+
+		//username
+		if (empty($username)){
+			$errors[] = "Please provide an username !";
+		}
+
+		//password
+		if (empty($password)){
+			$errors[] = "Please choose a password !";
+		}
+
+		//form valide ?
+		if (empty($errors)){
+			
+			//recherche l'utilisateur en bdd par son username (ou email)
+			$sql = "SELECT * FROM users
+					WHERE username = :login OR email = :login
+					LIMIT 1";
+
+			$stmt = $dbh->prepare($sql);
+			$stmt->bindValue(":login", $username);
+			$stmt->execute();
+
+			$user = $stmt->fetch();
+
+			$hashedPassword = hashPassword($password, $user['salt']);
+			if ($hashedPassword === $user['password']){
+				$_SESSION['user'] = $user;
+				header("Location: index.php");
+				die();
+			}
+		}
+	}
+?>
+
+
+
+<?php include("../inc/top.php");?>
+	<body>
+		<div id="wrapper">
+		<nav id="head">
+		
+			<a class="login" href="pages/login.php"><button>Log In</button></a>
+			<a class="home" href="pages/home.php"><button>Accueil</button></a>
+			
+			<h1>Stackoverflone</h1>
+			<input type="text" name="search" value="Search">
+			
+		</nav>
+		<div id="mainregister">
+			<div id="contentregister" id="form_sign">
+				<form method="POST" action="login.php" id="form_sign" method="POST" novalidate>
+					<label for="username">Username: 
+						<input type="text" name="username" value="<?php echo $username;?>"/>
+					</label><br/>
+					<label for="password">Password: 
+						<input type="password" name="password" value="<?php echo $password; ?>">
+					</label><br/>
+					<?php 
+						if (!empty($errors)){
+							echo '<ul class="errors">';
+							foreach($errors as $error){
+								echo '<li>'.$error.'</li>';
+							}
+							echo '</ul>';
+						}
+					?>
+					<input type="submit" value="Sign in!">
+				</form>
+			</div>
+			<div class="cnx">
+				<a  id="forgotten"href="password_reset_1.php" title="Forgot your password ?">Forgot your password ?</a>
+			</div>
+		</div>
+		<div id="copy">
+			<p>Site design/ logo &copy; <?php echo date("Y"); ?> staque corporation; user  contributions licensed under <a href=""> ccby-sa 3.0</a>
+		</div>		
+	</div>
+
+	<body>
+</html>
