3ba0c4d3b7f0a2176b7cdb2e1daea8a8f842234c
Deyris <ludovic.deyris@hotmail.fr>
2014-10-17 14:46:44 +0200

    password reset2 et 1

--- password_reset_1.php
+++ password_reset_1.php
@@ -1,113 +1,103 @@
 <?php
 
-	session_start();
-
-	include("db.php");
-	include("functions.php");
-
-	//déclaration des variables du formulaire
-	$email 		= "";
-
-	$errors = array();
-
-	//formulaire soumis ?
-	if (!empty($_POST)){
-
-		$email= $_POST['email'];
-
-		//vérifier que l'email existe bel et bien
-		$sql = "SELECT email, username, token FROM users
-				WHERE email = :email";
-		$stmt = $dbh->prepare($sql);
-		$stmt->bindValue(":email", $email);
-		$stmt->execute();
-		$user = $stmt->fetch();
-
-		if (!empty($user)){
-			//envoyer un message
-
-			include("phpMailer/PHPMailerAutoload.php"); //chargera les fichiers nécessaires
-
-			$mail = new PHPMailer();        //Crée un nouveau message (Objet PHPMailer)
-			$mail->CharSet = 'UTF-8';       //Encodage en utf8
-
-			//INFOS DE CONNEXION
-			$mail->isSMTP();                                    //On utilise SMTP
-			$mail->Username = "machinchoseformation@gmail.com"; //nom d'utilisateur
-			$mail->Password = "38Utc_Sd5KdI4sz0Gr2Y4g";         //mot de passe
-			$mail->Host = 'smtp.mandrillapp.com';               //smtp.gmail.com pour gmail
-			$mail->Port = 587;                                  //Le numéro de port
-			$mail->SMTPAuth = true;                             //On utilise l'authentification SMTP ?
-			//$mail->SMTPSecure = 'tls';                        //décommenter pour gmail
-
-			//CONFIGURATION DES PERSONNES
-			$mail->setFrom('account@wf3movies.com', 'WF3 Movies !');                   //qui envoie ce message ? (email, noms)
-			$mail->addReplyTo('account@wf3movies.com', 'WF3 Movies !');        //à qui répondre si on clique sur "reply" (email, noms)
-			$mail->addAddress($user['email'], $user['username']);   //destinataire
-			
-			//CONFIGURATION DU MESSAGE
-			$mail->isHTML(true);                                // Contenu du message au format HTML
-			$mail->Subject = 'Password reset on WF3 Movies !';         
+session_start();
+include("db.php");
+include("inc/functions.php");
 
 
-			$resetUrl = "http://localhost/movies/password_reset_2.php?email="
-			 . urlencode($email) . '&token=' . urlencode($user['token']);
+    //déclaration des variables du formulaire
+    $email      = "";
 
-			                       //le sujet
-			$mail->Body = 'Yo. Please click on the link below to reset your password:<br /><a href="'.$resetUrl.'">'.$resetUrl.'</a>';                                   //le message
+    $errors = array();
 
-			//envoie le message
-			if (!$mail->send()) {
-			    echo "Mailer Error: " . $mail->ErrorInfo;
-			} else {
-			    echo "Message sent!";
-			}
-		}
+    //formulaire soumis ?
+    if (!empty($_POST)){
 
+        $email= $_POST['email'];
 
-	}
+        if (empty($email)){
+            $errors[] = "Merci d'entrer votre email !";
+        }
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors[] = "Votre email n'est pâs valide !";
+        }
+
+        //vérifier que l'email existe bel et bien
+        $sql = "SELECT email, username, token FROM users
+                WHERE email = :email";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(":email", $email);
+        $stmt->execute();
+        $user = $stmt->fetch();
+
+        if (!empty($user)){
+            //envoyer un message
+
+            include("phpMailer/PHPMailerAutoload.php"); //chargera les fichiers nécessaires
+
+            $mail = new PHPMailer();        //Crée un nouveau message (Objet PHPMailer)
+            $mail->CharSet = 'UTF-8';       //Encodage en utf8
+
+            //INFOS DE CONNEXION
+            $mail->isSMTP();                                    //On utilise SMTP
+            $mail->Username = "nadeige.couthon@orange.fr"; //nom d'utilisateur
+            $mail->Password = "SUphwXNvhAEaSF1-G3020Q";         //mot de passe
+            $mail->Host = 'smtp.mandrillapp.com';               //smtp.gmail.com pour gmail
+            $mail->Port = 587;                                  //Le numéro de port
+            $mail->SMTPAuth = true;                             //On utilise l'authentification SMTP ?
+            //$mail->SMTPSecure = 'tls';                        //décommenter pour gmail
+
+    //CONFIGURATION DES PERSONNES
+    $mail->setFrom('account@staque.com', 'STAQUE');  //qui envoie ce message ? (email, noms)
+    $mail->addReplyTo('account@staque.com', 'STAQUE'); //à qui répondre si on clique sur "reply" (email, noms)
+    $mail->addAddress($user['email'], $user['username']);   //destinataire
+            
+
+    //CONFIGURATION DU MESSAGE
+    $mail->isHTML(true);                                // Contenu du message au format HTML
+    $mail->Subject = 'Password reset STAQUE'; //le sujet
+    
+    $resetUrl = "http://localhost/stack-overflow-clone/password_reset_2.php?email="
+             . urlencode($email) . '&token=' . urlencode($user['token']);
+
+    //le message
+
+    $mail->Body = 'Cliquez sur le lien suivant pour réinitialiser votre mot de passe :<br /><a href="'.$resetUrl.'">'.$resetUrl.'</a>';
+                    
+    //envoie le message
+    if (!$mail->send()) {
+        $errors[] = "Erreur lors de l'envoi du message";
+    }
+    else {
+        $errors[] = "Message envoyé !"; //$mailSent = true;
+        }
+    }
+}
+
+    include("inc/top.php");
 ?>
+<div class="container">
+    <form action="password_reset_1.php" id="password_reset_1" method="POST" novalidate >
+    <!-- peut servir pour détecter facilement QUEL formulaire a été soumis -->
+    <input type="hidden" name="form_name" value="password_reset_1" />
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>Films</title>
-	<meta name="description" content="PHP: Hypertext Preprocessor">
-	<link href="css/style.css" type="text/css" rel="stylesheet" />
+            <h3>OUBLI DU MOT DE PASSE !</h3>
 
-</head>
+    <div class="field_container">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" value="" />
+    </div>
+    <?php
+        if (!empty($errors)){
+            echo '<ul class="errors">';
+            foreach($errors as $error){
+                echo '<li>'.$error.'</li>';
+            }
+            echo '</ul>';
+        }
+    ?>
+    <input type="submit" value="REINITIALISER VOTRE MOT DE PASSE" class="submit"/>
 
-	<body>
-		<div id="mainregister">
-			<div id="contentregister">
-				<form method="POST" action="password_reset_1.php" method="POST" novalidate>
-					<input type="hidden" name="forn_name" value="password_reset_1" />
-					<h3>Mot de passe oublié?</h3>
-					<label for="email">email: 
-						<input type="text" name="email" value="<?php echo $email;?>"/>
-					</label><br/>
-					
-					<?php 
-						if (!empty($errors)){
-							echo '<ul class="errors">';
-							foreach($errors as $error){
-								echo '<li>'.$error.'</li>';
-							}
-							echo '</ul>';
-						}
-					?>
-					<input type="submit" value="Envoyer">
-				</form>
-			</div>
-			<div class="cnx">
-				<a  id="forgotten2"href="login.php" title="Login">Login</a>
-				<a id="btn3" href="index.php">Retour accueil</a>
-
-		</div>
-		</div>
-		<script src="js/jquery.js"></script>
-		<script src="js/app.js"></script>
-
-	<body>
-</html>
+</form>
+</div>
+<?php   include("inc/bottom.php"); ?>
--- password_reset_2.php
+++ password_reset_2.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	include("db.php");
-	include("functions.php");
+	include("inc/functions.php");
 
 	$email = "";
 	if (!empty($_GET['email'])){
@@ -31,7 +31,7 @@
 
 	//déclaration des variables du formulaire
 	$password 		= "";
-	$password_bis  	= "";
+	$password_bis 	= "";
 
 	$errors = array();
 
@@ -41,22 +41,22 @@
 		$password 		= $_POST["password"];
 		$email 		= $_POST["email"];
 		$token 		= $_POST["token"];
-		$password_bis  	= $_POST['password_bis '];
+		$password_bis 	= $_POST['password_bis'];
 
 
 		//password
-		if (empty($password)){
-			$errors[] = "Please choose a password !";
-		}
-		elseif (empty($password_bis)){
-			$errors[] = "Please confirm your password !";
-		}
-		elseif ($password_bis != $password){
-			$errors[] = "Your passwords do not match !";
-		}
-		elseif (strlen($password) < 7){
-			$errors[] = "Your password should have at least 7 characters !";
-		}
+        if (empty($password)){
+            $errors[] = "Choisissez un Mot de passe !";
+        }
+        elseif (empty($password_bis)){
+            $errors[] = "Merci de confirmer votre mot de passe !";
+        }
+        elseif ($password_bis != $password){
+            $errors[] = "Les deux mots de passe ne correspondent pas !";
+        }
+        elseif (strlen($password) < 7){
+            $errors[] = "Votre mot de passe doit contebnir au moins 7 caractères !";
+        }
 
 		if (empty($errors)){
 			
@@ -89,54 +89,36 @@
 		}
 	}
 
-	
+	include("inc/top.php");
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>Films</title>
-	<meta name="description" content="PHP: Hypertext Preprocessor">
-	<link href="css/style.css" type="text/css" rel="stylesheet" />
+<div class="container">
+<form name="yo" action="password_reset_2.php?<?php echo $_SERVER['QUERY_STRING']; ?>" id="password_reset_2" method="POST" novalidate>
 
-</head>
+	<!-- peut servir pour détecter facilement QUEL formulaire a été soumis -->
+	<input type="hidden" name="form_name" value="password_reset_2" />
+	<input type="hidden" name="token" value="<?= $token ?>" />
+	<input type="hidden" name="email" value="<?= $email ?>" />
 
-	<body>
-		<div id="mainregister">
-			<div id="contentregister">
-				<form method="POST" action="password_reset_2.php?<?php echo $_SERVER['QUERY_STRING']; ?>" method="POST" novalidate>
-					<input type="hidden" name="forn_name" value="password_reset_1" />
-					<!-- peut servir pour détecter facilement QUEL formulaire a été soumis -->
-					<input type="hidden" name="forn_name" value="password_reset_2" />
-					<input type="hidden" name="token" value="<?= $token ?>" />
-					<input type="hidden" name="email" value="<?= $email ?>" />
-					<h3>Nouveau mot de passe<h3>
-					<label for="passwod">New password: 
-						<input type="password" name="password" value=""/>
-					</label><br/>
-					<label for="password_bis">confirmer: 
-						<input type="text" name="$password_bis " value=""/>
-					</label><br/>
-					<?php 
-						if (!empty($errors)){
-							echo '<ul class="errors">';
-							foreach($errors as $error){
-								echo '<li>'.$error.'</li>';
-							}
-							echo '</ul>';
-						}
-					?>
-					<input type="submit" value="Save">
-				</form>
-			</div>
-			<div class="cnx">
-				<a  id="forgotten2"href="login.php" title="Login">Login</a>
-				<a id="btn3" href="index.php">Retour accueil</a>
+	<h3>FORGOT YOUR PASSWORD ?</h3>
 
-		</div>
-		</div>
-		<script src="js/jquery.js"></script>
-		<script src="js/app.js"></script>
-
-	<body>
-</html>
+	<div class="field_container">
+		<label for="password">Nouveau Mot de passe</label>
+		<input type="password" id="password" name="password" value="" />
+	</div>
+	<div class="field_container">
+		<label for="password_bis">Confirmez</label>
+		<input type="password" id="password_bis" name="password_bis" value="" />
+	</div>
+	<?php 
+		if (!empty($errors)){
+			echo '<ul class="errors">';
+			foreach($errors as $error){
+				echo '<li>'.$error.'</li>';
+			}
+			echo '</ul>';
+		}
+	?>
+	<input type="submit" value="SAVE !" class="submit"/>
+</form>
+</div>
+<?php 	include("inc/bottom.php"); ?>
